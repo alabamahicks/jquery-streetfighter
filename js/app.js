d16b4eaa02a712ce8b6d7478f0dae04e1a6e2b2e
Sarah Hicks <sarah.hicks@emerson.com>
2015-09-18 12:02:12 -0500

    Progress thru 2.2.3
    Mouseenter and mouseleave hide/show

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+
+	$('.ryu').mouseenter(function() {
+		// alert('mouse entered .ryu div');
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+
+	})
+	.mouseleave(function() {
+		// alert('mouse left');
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+
+	;
+
+});
